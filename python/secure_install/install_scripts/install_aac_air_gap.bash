2924b27c5edc1834490fa470ecbb23b4a8510c9e
Lizz Condrey <ashley.condrey@ngc.com>
2024-05-01 21:32:20 +0000

    - correcting install scripts to alleviate lower dependency conflict from including the extras in the pyproject.toml [all]

--- python/secure_install/install_scripts/install_aac_air_gap.bash
+++ python/secure_install/install_scripts/install_aac_air_gap.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# This script will install AaC and its dependencies via the wheel files.
+# This script will install AaC Specifications and its dependencies via the wheel files.
 
 install_script_dir=$(dirname "$0")
 cd $install_script_dir
 
-pip install --require-hashes -r requirements.txt --no-index --find-links ./
+pip install --require-hashes --no-deps -r requirements.txt --no-index --find-links ./
--- python/secure_install/install_scripts/install_aac_verified_index.bat
+++ python/secure_install/install_scripts/install_aac_verified_index.bat
@@ -1,7 +1,7 @@
-REM This script will install AaC and its dependencies via PyPI.
+REM This script will install AaC Specifications and its dependencies via PyPI.
 
 @echo off
 
 cd /D "%~dp0"
 
-python -m pip "install" "--require-hashes" "-r" "requirements.txt"
+python -m pip "install" "--require-hashes" "--no-deps" "-r" "requirements.txt"
